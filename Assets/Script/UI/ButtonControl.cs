f818facabf0ad4ce551ac1c8e73e2ddcb1235bbb
ERIC KAO <kaoch@deakin.edu.au>
2019-12-10 17:15:59 +1100

    Implement cutscenes

---
+++ Assets/Script/UI/ButtonControl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ButtonControl : MonoBehaviour
+{
+    public void StartButton_Click()
+    {
+        FindCanvasGroup("CanvasS1").Hide();
+        FindCanvasGroup("CanvasS2").Show();
+    }
+    public void SettingButton_Click()
+    {
+        Debug.Log("Hello World!");
+    }
+    public void NextButton_Click(int nextNumber)
+    {
+        Canvas canvas = GetComponentInParent<Canvas>();
+        CanvasGroup canvasGroup = canvas.GetComponent<CanvasGroup>();
+        canvasGroup.Hide();
+        FindCanvasGroup("CanvasS" + nextNumber).Show();
+    }
+    public CanvasGroup FindCanvasGroup(string name)
+    {
+        return GameObject.Find(name).GetComponent<CanvasGroup>();
+    }
+}
