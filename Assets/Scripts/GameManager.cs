baf64a2d77db46d9b2d461f3eda8a688ac7f09cd
ERIC KAO <kaoch@deakin.edu.au>
2019-12-19 10:58:55 +1100

    Refactor game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class GameManager : MonoBehaviour
+public class GameManager
 {
+    private static GameManager instance;
     private string _currentCanvas;
     [SerializeField]
     private float _maxLife;
@@ -16,42 +17,50 @@ public class GameManager : MonoBehaviour
             _currentLife = value;
             if (_currentLife <= 0)
             {
-                SceneManager.LoadScene("CutScene");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
         }
     }
     public float MaxLife { get => _maxLife; }
-    void Start()
+    private GameManager()
     {
-        DontDestroyOnLoad(gameObject);
-        if (_maxLife == 0) _maxLife = 1;
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+        _maxLife = 2;
         _currentLife = MaxLife;
+    }
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameManager();
+            }
+            return instance;
+        }
     }
     public void SwitchScene(string currentCanvas, string targetScene)
     {
         _currentCanvas = currentCanvas;
+        Debug.Log(targetScene);
         SceneManager.LoadScene(targetScene);
     }
     public void SwitchScene(string targetScene)
     {
         SceneManager.LoadScene(targetScene);
     }
-    void OnEnable()
-    {
-        SceneManager.sceneLoaded += OnLevelFinishedLoading;
-    }
-    void OnDisable()
-    {
-        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
-    }
     void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
     {
         switch (scene.name)
         {
             case "CutScene":
-                if (_currentCanvas == null) return;
+                if (_currentCanvas == null) _currentCanvas = "CanvasS9";
                 GameObject.Find("CanvasS1").GetComponent<CanvasGroup>().Hide();
                 GameObject.Find(_currentCanvas).GetComponent<CanvasGroup>().Show();
+                break;
+            default:
+                _maxLife = 2;
+                _currentLife = MaxLife;
                 break;
         }
     }
