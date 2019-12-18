2636eda2a504dda613973ee41cc0587a347273e2
ERIC KAO <kaoch@deakin.edu.au>
2019-12-18 11:25:06 +1100

    Update cutscenes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    private string _currentCanvas;
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    public void SwitchScene(string currentCanvas, string targetScene)
+    {
+        _currentCanvas = currentCanvas;
+        SceneManager.LoadScene(targetScene);
+    }
+    public void SwitchScene(string targetScene)
+    {
+        SceneManager.LoadScene(targetScene);
+        GameObject.Find("CanvasS1").GetComponent<CanvasGroup>().Hide();
+        GameObject.Find(targetScene).GetComponent<CanvasGroup>().Show();
+    }
+}
